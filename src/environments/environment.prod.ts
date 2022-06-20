0065959a1b5cc8ee47c741170b916981bfeb98b3
José Almir <jr.cod.dev@gmail.com>
2022-06-20 09:50:28 -0300

    Setup angular with firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'soul-digital-4a4a4',
+    appId: '1:878621959406:web:9c1dd07f73798732677fff',
+    storageBucket: 'soul-digital-4a4a4.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBv8-0YI9hl4Km7LjpMSRff72mxAQCLQRk',
+    authDomain: 'soul-digital-4a4a4.firebaseapp.com',
+    messagingSenderId: '878621959406',
+  },
   production: true
 };
